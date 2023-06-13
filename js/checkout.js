e52a1cf35443b9d3aeae6561854fe82305375857
VladScv <ddcllrnsbrv@uoc.edu>
2023-06-13 11:16:09 +0200

    Fifth commit: Level 1 complete and code refactored

--- js/checkout.js
+++ js/checkout.js
@@ -1,23 +1,52 @@
-
+function checkPassword(password){
+	let res=0;
+	if(password.search(/[a-z]/i)>=0)res++;
+	if(password.search(/[0-9]/)>=0)res++;
+	return res===2;
+}
+function checkMail(mail){
+	return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail));
+}
 // Exercise 6
 function validate() {
 	var error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	let fields=[
+		{field:'fName', element:document.getElementById("fName")},
+		{field:'fLastN', element:document.getElementById("fLastN")},
+		{field:'fEmail', element:document.getElementById("fEmail")},
+		{field:'fAddress', element:document.getElementById("fAddress")},
+		{field:'fPassword', element:document.getElementById("fPassword")}
+	];
 
+	function markError(item) {
+		error++;
+		item.element.className += ' is-invalid';
+	}
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
-	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
-		error++;
-	}
+	 errorName : document.getElementById("errorName");
+	 errorEmail : document.getElementById("errorEmail");
 
-	if(fEmail.value == ""){
-		error++;
-	}
+
+
+// Validate fields entered by the user: name, phone, password, and email
+	fields.forEach(item=>{
+		if(item.element.value===""||item.element.value.length<3){
+			markError(item);
+		}else {
+			switch (item.field) {
+				case 'fName':
+				case 'fLastN':
+					if(item.element.value.search(/[a-z]/i)<0)markError(item);
+					break;
+				case 'fEmail':
+					if (!checkMail(item.element.value))markError(item);
+					break;
+				case 'fPassword':if (!checkPassword(item.element.value))markError(item)
+			}
+		}
+
+	})
 	 
 	if(error>0){
 		alert("Error");
--- js/shop.js
+++ js/shop.js
@@ -11,13 +11,13 @@ fetch('./products.json')
     });
 
 
-
 // Exercise 1
 function buy(id) {
     if(jsonLoaded)
         products.forEach(function (p){
             if (p.id === id) {
                 cartList.push(p)
+                document.getElementById('count_product').innerText= ''+cartList.length;
                 console.log(p.name + " added to cart")
             }
         })
@@ -30,10 +30,10 @@ function cleanCart() {
 // Exercise 3
 function calculateTotal() {
     let res=0;
-    for (let i = 0; i < cartList.length; i++) {
-        res+=cartList[i].price;
-    }
-   total=res;
+    cart.forEach(item => {
+        res+= (item.subtotalWithDiscount>0?item.subtotalWithDiscount:item.subtotal)
+    });
+   return total=res;
 }
 
 // Exercise 4
@@ -44,28 +44,51 @@ function generateCart() {
         cart.forEach(item => {
             if(item.id===cartList[i].id){
                 item.quantity++;
+                item.subtotal+=cartList[i].price
                 found=true;
             }
         });
         }
         if(!found){
-            cartList[i]['quantity']=1
-            cart.push({"qtt":1,"product":cartList[i]})
+            cart.push(cartList[i])
+            let pos= cart.length-1;
+            cart[pos]['quantity']=1
+            cart[pos]['subtotal']=cart[pos].price;
+            cart[pos]['subtotalWithDiscount']=0;
         }
     }
 }
 
 // Exercise 5
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+    cart.forEach((item)=>{
+        if(item.offer && item.quantity>=item.offer.number){
+            let aux= item.subtotal * (1-item.offer.percent/100)
+            item.subtotalWithDiscount= Math.round(aux * 100)/100
+        }
+    })
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    let totalPrice=0;
+    let list= document.getElementById('cart_list')
+    let html=''
+    generateCart();
+    applyPromotionsCart();
+    cart.forEach(item=>{
+        html += "<th scope='row'>" + item.name+"</th>" +
+            "<td>$"+item.price+"</td><td>"+
+            item.quantity+"</td><td>$"+
+            (item.subtotalWithDiscount?item.subtotalWithDiscount:item.subtotal)+
+            "</td></tr>";
+        totalPrice+= (item.subtotalWithDiscount?item.subtotalWithDiscount:item.subtotal);
+    })
+    document.getElementById('total_price').innerText=''+calculateTotal();
+    list.innerHTML=html;
+    document.getElementById('cartModal')
 }
-
-
 // ** Nivell II **
 
 // Exercise 7
@@ -73,6 +96,22 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    if(jsonLoaded)
+        products.forEach(function (p){
+            if (p.id === id) {
+                cartList.push(p)
+                let found=false;
+                for (let i = 0; i < cart.length; i++) {
+                    if(cart[i].id===id){
+                        cart[i].quantity++;
+                        found=true;
+                    }
+                }
+                if(!found) cart.push(p)
+                document.getElementById('count_product').innerText= ''+cartList.length;
+                console.log(p.name + " added to cart")
+            }
+        })
 }
 
 // Exercise 8
