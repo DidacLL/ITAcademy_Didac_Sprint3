a974484e785924f5129c814a6b6a4b31d026da25
VladScv <ddcllrnsbrv@uoc.edu>
2023-06-13 12:00:58 +0200

    Sixth commit: Level 2 complete and code refactored

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 let jsonLoaded = false;
-let products, cartList = [], cart = [], total = 0;
+let products,  cart = [], total = 0,cartCounter=0;
 fetch('./products.json')
     .then(response => response.json())
     .then(data => {
@@ -11,32 +11,48 @@ fetch('./products.json')
     });
 
 
+function updateCartCounter() {
+    document.getElementById('count_product').innerText = cartCounter;
+}
 // Exercise 1
+/**
+ * @Deprecated Since 5th commit, now using addToCart()
+ * */
 function buy(id) {
     if(jsonLoaded)
         products.forEach(function (p){
             if (p.id === id) {
                 cartList.push(p)
-                document.getElementById('count_product').innerText= ''+cartList.length;
+                document.getElementById('count_product').innerText= ''+ cartList.length;
                 console.log(p.name + " added to cart")
             }
         })
 }
 // Exercise 2
-function cleanCart() {
-    cartList.length=0;
-}
 
+function cleanCart() {
+
+    cartCounter=0;
+    cart.length=0;
+    updateCartCounter();
+    printCart();
+}
 // Exercise 3
+
 function calculateTotal() {
     let res=0;
+    cartCounter=0;
     cart.forEach(item => {
+        cartCounter+= item.quantity;
         res+= (item.subtotalWithDiscount>0?item.subtotalWithDiscount:item.subtotal)
     });
    return total=res;
 }
-
 // Exercise 4
+
+/**
+ * @Deprecated Since 5th commit, now using addToCart()
+ * */
 function generateCart() {
     for (let i = 0; i < cartList.length; i++) {
         let found=false;
@@ -58,8 +74,8 @@ function generateCart() {
         }
     }
 }
-
 // Exercise 5
+
 function applyPromotionsCart() {
     cart.forEach((item)=>{
         if(item.offer && item.quantity>=item.offer.number){
@@ -68,21 +84,19 @@ function applyPromotionsCart() {
         }
     })
 }
-
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
     let totalPrice=0;
     let list= document.getElementById('cart_list')
     let html=''
-    generateCart();
     applyPromotionsCart();
     cart.forEach(item=>{
-        html += "<th scope='row'>" + item.name+"</th>" +
-            "<td>$"+item.price+"</td><td>"+
-            item.quantity+"</td><td>$"+
-            (item.subtotalWithDiscount?item.subtotalWithDiscount:item.subtotal)+
-            "</td></tr>";
+        html += "<tr><th scope='row'>" + item.name+"</th>" +
+            "<td>$"+ item.price+ "</td>" +
+            "<td>"+item.quantity+"</td>" +
+            "<td>$"+(item.subtotalWithDiscount?item.subtotalWithDiscount:item.subtotal)+"</td>" +
+            "<td><button type='button' class='btn btn-close' onclick=removeFromCart("+item.id+")></button></td>";
         totalPrice+= (item.subtotalWithDiscount?item.subtotalWithDiscount:item.subtotal);
     })
     document.getElementById('total_price').innerText=''+calculateTotal();
@@ -90,6 +104,7 @@ function printCart() {
     document.getElementById('cartModal')
 }
 // ** Nivell II **
+
 
 // Exercise 7
 function addToCart(id) {
@@ -99,16 +114,24 @@ function addToCart(id) {
     if(jsonLoaded)
         products.forEach(function (p){
             if (p.id === id) {
-                cartList.push(p)
+                // cartList.push(p)
+                cartCounter++;
                 let found=false;
                 for (let i = 0; i < cart.length; i++) {
                     if(cart[i].id===id){
                         cart[i].quantity++;
+                        cart[i].subtotal+=p.price;
                         found=true;
                     }
                 }
-                if(!found) cart.push(p)
-                document.getElementById('count_product').innerText= ''+cartList.length;
+                if(!found){
+                   p['quantity']=1
+                   p['subtotal']=p.price;
+                   p['subtotalWithDiscount']=0;
+                    cart.push(p)
+
+                } 
+                updateCartCounter();
                 console.log(p.name + " added to cart")
             }
         })
@@ -116,8 +139,21 @@ function addToCart(id) {
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for (let i = 0; i < cart.length; i++) {
+        if(cart[i].id===id){
+            cartCounter--;
+            updateCartCounter();
+            if (cart[i].quantity>1){
+                cart[i].quantity--;
+                cart[i].subtotal-=cart[i].price;
+            }
+            else cart.splice(i,1)
+            calculateTotal();
+            applyPromotionsCart();
+            printCart();
+            return;
+        }
+    }
 }
 
 function open_modal(){
